d475eacfcc45ffc5ad8dae2f4e40a6ffcf309c1a
Aman <amancoronel0397@gmail.com>
2021-04-02 20:14:51 +0800

    Remove token checking

--- resources/functions/tokens.js
+++ resources/functions/tokens.js
@@ -8,15 +8,12 @@ exports.getAuthId = async (info) => {
 
 exports.verifyToken = async (req, res, next) => {
     try {
-        let bearer = req.get("Authorization");
-        // console.log("**** ", bearer);
-        if(!bearer) throw "Session Expired"; 
+        // let bearer = req.get("Authorization");
+        // // console.log("**** ", bearer);
+        // if(!bearer) throw "Session Expired"; 
 
-        let token = bearer.split(" ")[1];
-        if(req.session.token != token) throw "Session Expired" //CHANGE THIS TO REQ.SESSION
-        // console.log("***", token, req.session);
-        // let payload = jwt.verify(token, process.env.TOKEN_SECRET);
-        // if(!payload || payload.user.email != req.params.email) throw "Session Expired";
+        // let token = bearer.split(" ")[1];
+        // if(req.session.token != token) throw "Session Expired" //CHANGE THIS TO REQ.SESSION
 
         next();
     } catch(err) {
