ebaa17445657bdb3ec7048b8dc7a3603f461d3c8
Aman <amancoronel0397@gmail.com>
2021-04-02 15:38:48 +0800

    Change config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config(); // RUN DOT ENV TO GET .env FILE CONFIGURATION
 const loader = require('./loader'); //LOAD ALL DATA TO CLASSES
-const port = process.env.DEV_PORT || 8080;
+const port = process.env.PORT || 3000;
 
 require('./middleware')(app);
 require('./routes')(app);
