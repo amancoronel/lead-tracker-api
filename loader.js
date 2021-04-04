968c8c71b0947dd6218f0c02a780d0d34aed1728
Aman <amancoronel0397@gmail.com>
2021-04-04 20:17:15 +0800

    Wrong name

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@ const Classes = require('./resources/classes');
 const dataParser = require('./data');
 
 module.exports = async () => {
-    Classes.addresses.data = await dataParser.readFile('address');
+    Classes.addresses.data = await dataParser.readFile('addresses');
     Classes.agents.data = await dataParser.readFile('agents');
     Classes.contractors.data = await dataParser.readFile('contractors');
     Classes.lenders.data = await dataParser.readFile('lenders');
