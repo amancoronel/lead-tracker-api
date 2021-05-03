e5190429ccdde7c4951b8625de765a37e8088221
Aman <amancoronel0397@gmail.com>
2021-05-03 12:08:44 +0800

    Add project in aggregate

--- resources/functions/db.js
+++ resources/functions/db.js
@@ -61,6 +61,18 @@ exports.getLeadData = async (condition = {}) => {
             },
             {
                 $unwind: "$lender_data",
+            },
+            {
+                "$project" : {
+                    "_id" : "$_id",
+                    "lead_number" : "$lead_number",
+                    "agent_id": 1,
+                    "contractor_id": 1,
+                    "lender_id": 1,
+                    "agent_data" : { "$concat" : ["$agent_data.first_name"," ","$agent_data.last_name"] } ,
+                    "contractor_data" : "$contractor_data.contractor_name",
+                    "lender_data": "$lender_data.lender_name"
+                } 
             }
         ])
         .then(data => resolve(data))
